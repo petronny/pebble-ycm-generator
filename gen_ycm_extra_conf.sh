2682fd7197871396a9435f206daf629242b75157
Jingbei Li <i@jingbei.li>
2016-11-05 22:08:56 +0800

    Update gen_ycm_extra_conf.sh

--- gen_ycm_extra_conf.sh
+++ gen_ycm_extra_conf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export CC=arm-none-eabi-gcc
+
 MAKEFILE="makefile.ycm"
 rm -f .ycm_extra_conf.py
 pebble clean 1>/dev/null 2>&1
@@ -7,14 +9,15 @@ pebble build -v 2>&1 | grep runner >> $MAKEFILE
 sed \
 	-e 's/\[//g' \
 	-e 's/\]//g' \
-	-e "s/[']//g" \
+	-e "s/ u'/ '/g" \
+	-e "s/'//g" \
 	-e "s/, / /g" \
 	-e 's/^.*runner/\tcd build;/g' \
 	-e 's/-D__FILE_NAME__="\([^"]*\)"/-D__FILE_NAME__=\\"\1\\"/g' \
 	-e 's/arm-none-eabi-gcc/$(CC)/g' \
 	-i $MAKEFILE
-CC=arm-none-eabi-gcc make -f $MAKEFILE
-CC=arm-none-eabi-gcc ycm_generator -b make -M="-f $MAKEFILE" .
+make -f $MAKEFILE
+ycm_generator -b make -M="-f $MAKEFILE" .
 sed \
 	-e '/-D__FILE_NAME__/d' \
 	-e '/-DPBL_DISPLAY_WIDTH/d' \
